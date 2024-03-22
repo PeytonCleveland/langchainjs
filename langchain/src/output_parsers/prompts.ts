824c3418de82393b24f3047651c9448690929485
Shawn Contant <shawn.contant@omnifederal.com>
2024-03-22 10:01:35 -0500

    Why is this test failing???

--- langchain/src/output_parsers/prompts.ts
+++ langchain/src/output_parsers/prompts.ts
@@ -9,18 +9,13 @@ Completion:
 {completion}
 --------------
 
-Above, the Completion did not satisfy the constraints given in
-the Instructions.
-
+Above, the Completion did not satisfy the constraints given in the Instructions.
 Error:
-
 --------------
 {error}
-
 --------------
 
-Please try again. Please only respond with an answer that
-satisfies the constraints laid out in the Instructions:`;
+Please try again. Please only respond with an answer that satisfies the constraints laid out in the Instructions:`;
 
 export const NAIVE_FIX_PROMPT =
   /* #__PURE__ */ PromptTemplate.fromTemplate(NAIVE_FIX_TEMPLATE);
